5abab8c3afe4cb27b6f42ece36eea830af745954
Michelle Chen <mich.chen.94@gmail.com>
2020-10-06 15:28:27 -0700

    initial commit

---
+++ height-binary-tree/height_binary_tree.py
@@ -0,0 +1,45 @@
+class Node:
+    def __init__(self, info): 
+        self.info = info  
+        self.left = None  
+        self.right = None 
+        self.level = None 
+
+    def __str__(self):
+        return str(self.info) 
+
+class BinarySearchTree:
+    def __init__(self): 
+        self.root = None
+
+    def create(self, val):  
+        if self.root == None:
+            self.root = Node(val)
+        else:
+            current = self.root
+         
+            while True:
+                if val < current.info:
+                    if current.left:
+                        current = current.left
+                    else:
+                        current.left = Node(val)
+                        break
+                elif val > current.info:
+                    if current.right:
+                        current = current.right
+                    else:
+                        current.right = Node(val)
+                        break
+                else:
+                    break
+
+
+def height(root):
+
+    left_height = 0
+    right_height = 0
+
+    if root is None:
+        return 0
+
