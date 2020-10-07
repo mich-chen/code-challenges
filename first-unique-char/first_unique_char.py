b07d4fceeac07bd43d926598423c6711e098c7dc
Michelle Chen <mich.chen.94@gmail.com>
2020-10-06 21:41:50 -0700

    using builtin count() function

--- first-unique-char/first_unique_char.py
+++ first-unique-char/first_unique_char.py
@@ -1,9 +1,18 @@
 def firstUniqChar(s: str) -> int:
     """Return index of first non-repeating character in string."""
 
+    for i in range(len(s)):
+        count = s.count(s[i])
+        if count == 1:
+            return i
+
+    return -1
+
+    # O(n)
+
 
 """
-Alternative solution: dictionary/hashmap, no builtin methods
+Alternative solution: dictionary/hashmap, no builtin methods O(n)
 
 def firstUniqChar(s: str) -> int:
 
@@ -20,3 +29,7 @@ def firstUniqChar(s: str) -> int:
     return -1
 """
 
+if __name__ == '__main__':
+    
+    print(firstUniqChar("loveleetcode"))  # 2
+    print(firstUniqChar("treestraversing"))  # 7
