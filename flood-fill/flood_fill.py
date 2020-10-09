5e02e2a3bc20ae9cf74794e232864a50c2364cbf
Michelle Chen <mich.chen.94@gmail.com>
2020-10-08 18:14:02 -0700

    add helper dfs function to be called recursively

---
+++ flood-fill/flood_fill.py
@@ -0,0 +1,39 @@
+from typing import List
+
+def floodFill(self, image: List[List[int]], sr: int, sc: int, newColor: int) -> List[List[int]]:
+        
+        """
+        connected 4-directionally --> horizontal, vertical, no diagonal
+        given starting pixel
+        starting pixel's current fill will tbe the condition we need to look for for neighboring pixels
+            know what the current fill is
+        
+        """
+
+        rows = len(image) 
+        cols = len(image[0]) # num of columns from first row
+        # condition we are looking for 
+        curr_fill = image[sr][sc] 
+
+        def dfs(row, col):
+            """Use DFS until each end of same curr_fill color"""
+
+            # if < 0 and >= rows or cols --> means out of bounds
+            if row < 0 or row >= rows or col < 0  or col >= cols:
+                return
+            # if pixel is not same as old color, and if pixel is new color
+            # then don't need to search further
+            # == newColor accounts for visited pixels
+            if image[row][col] != curr_fill or image[row][col] == newColor:
+                return
+            # print(row, col)
+            image[row][col] = newColor
+            # call dfs recursively on all 4-directional neighbors
+            dfs(row - 1, col)
+            dfs(row + 1, col)
+            dfs(row, col - 1)
+            dfs(row, col + 1)
+            
+        dfs(sr,sc)
+            
+        return image
