41299ea013115d3f94ceda6ae4a3b14bdb9b86f6
Michelle Chen <mich.chen.94@gmail.com>
2020-10-10 15:06:25 -0700

    completed helper dfs function

--- subsets/subsets.py
+++ subsets/subsets.py
@@ -3,7 +3,17 @@ from typing import List
 # ***** DFS recursively *****
 class Solution:
 
-    def dfs():
+    def dfs(nums, subset, output):
+        # add input subset to output
+        output.append(subset)
+
+        # iterate over input list
+        for i in range(len(nums)):
+            # traverse down subset tree's branch, 
+            # new subset is concatenated current subset + current iteration's value
+            self.dfs(num[i+1:], subset + [nums[i]], output)
+
+        # for-loop will not execute when input nums is empty list
 
     def subsets(nums: List[int]) -> List[List[int]]:
 
@@ -12,3 +22,8 @@ class Solution:
         # start dfs recursion with base case: empty list
         self.dfs(nums, [], output)
         return output
+
+
+if __name__ == '__main__':
+    
+    
