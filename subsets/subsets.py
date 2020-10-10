dfbdb347e57b433bb7f2393285ab82a6b9c2d2cb
Michelle Chen <mich.chen.94@gmail.com>
2020-10-10 15:10:12 -0700

    fixed typos, add test case

--- subsets/subsets.py
+++ subsets/subsets.py
@@ -3,7 +3,7 @@ from typing import List
 # ***** DFS recursively *****
 class Solution:
 
-    def dfs(nums, subset, output):
+    def dfs(self, nums, subset, output):
         # add input subset to output
         output.append(subset)
 
@@ -11,19 +11,21 @@ class Solution:
         for i in range(len(nums)):
             # traverse down subset tree's branch, 
             # new subset is concatenated current subset + current iteration's value
-            self.dfs(num[i+1:], subset + [nums[i]], output)
+            self.dfs(nums[i+1:], subset + [nums[i]], output)
 
         # for-loop will not execute when input nums is empty list
 
-    def subsets(nums: List[int]) -> List[List[int]]:
+    def subsets(self, nums: List[int]) -> List[List[int]]:
 
         # 2d array with all possible subsets
         output = []
         # start dfs recursion with base case: empty list
         self.dfs(nums, [], output)
-        return output
+        return sorted(output)
 
 
 if __name__ == '__main__':
     
-    
+    soln = Solution()
+    print(soln.subsets([1,2,3])) 
+    [[], [1], [1, 2], [1, 2, 3], [1, 3], [2], [2, 3], [3]]
