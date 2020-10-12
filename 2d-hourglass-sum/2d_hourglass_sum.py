faf8bb51dacd7d0f256a55e592937da85b711703
Michelle Chen <mich.chen.94@gmail.com>
2020-10-12 16:41:25 -0700

    set max_hg to be first hourglass, then iterate through 2d array

--- 2d-hourglass-sum/2d_hourglass_sum.py
+++ 2d-hourglass-sum/2d_hourglass_sum.py
@@ -15,7 +15,10 @@ def hourglassSum(arr):
     """
 
     # keep track of max hourglass, and compare and update with each iteration through hourglasses; max(max_hg, current_hg)
-    max_hg = 0
+    # set first hourglass as max_hg
+    max_hg = arr[0][0] + arr[0][1] + arr[0][2]\
+           + arr[1][1]\
+           + arr[2][0] + arr[2][1] + arr[2][2]
     
     # don't need to look at last two because top of hourglass is 3 squares
     for i in range(6 - 2):
@@ -27,3 +30,15 @@ def hourglassSum(arr):
             max_hg = max(max_hg, current_hg)
 
     return max_hg
+
+
+if __name__ == '__main__':
+    
+    print(hourglassSum([
+        [-1, -1, 0, -9, -2, -2],
+        [-2, -1, -6, -8, -2, -5],
+        [-1, -1, -1, -2, -3, -4],
+        [-1, -9, -2, -4, -4, -5],
+        [-7, -3, -3, -2, -9, -9],
+        [-1, -3, -1, -2, -4, -5]
+        ]))  # -6
