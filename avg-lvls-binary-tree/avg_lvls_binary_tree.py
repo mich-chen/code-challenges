820f77cd57587921ac91dd92e9cb165ef1984136
Michelle Chen <mich.chen.94@gmail.com>
2020-10-22 23:00:07 -0700

    DFS + recursion solution

--- avg-lvls-binary-tree/avg_lvls_binary_tree.py
+++ avg-lvls-binary-tree/avg_lvls_binary_tree.py
@@ -29,6 +29,33 @@ def averageOfLevels(root: TreeNode) -> List[float]:
     return output
 
 
+    """
+    # ***** DFS *****
+    output = []
+    tracker = {} 
+    
+    def dfs(root, level):
+        # base case
+        # if empty tree
+        if not root:
+            return
+        
+        tracker.setdefault(level, [])
+        tracker[level].append(root.val)
+        dfs(root.left, level + 1) 
+        dfs(root.right, level + 1) 
+        
+    dfs(root, 0)
+    # print(tracker)
+    for i in range(len(tracker)):
+        length = len(tracker[i])
+        avg = sum(num for num in tracker[i]) / length
+        output.append(avg)
+    
+    return output
+    """
+
+
 # ***** Test cases *****
 # Input: [3,9,20,15,7]
 # Output: [3, 14.5, 11]
