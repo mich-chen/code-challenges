8374479ad3a4080c100b13fe1caaf7a7c9e4b476
Michelle Chen <mich.chen.94@gmail.com>
2020-10-22 22:05:35 -0700

    BFS solution

---
+++ avg-lvls-binary-tree/avg_lvls_binary_tree.py
@@ -0,0 +1,34 @@
+# Definition for a binary tree node.
+# class TreeNode:
+#     def __init__(self, val=0, left=None, right=None):
+#         self.val = val
+#         self.left = left
+#         self.right = right
+import collections
+def averageOfLevels(root: TreeNode) -> List[float]:
+
+    # BFS
+    output = []
+    # queue of tuples for node, and level
+    queue = collections.deque([root])
+    
+    while queue:
+        avg = 0
+        temp = []
+        length = len(queue)
+        for i in range(len(queue)):
+            current = queue.popleft()
+            avg += current.val
+            if current.left:
+                temp.append(current.left)
+            if current.right:
+                temp.append(current.right)
+        output.append(avg / length)
+        queue.extend(temp)
+        
+    return output
+
+
+# ***** Test cases *****
+# Input: [3,9,20,15,7]
+# Output: [3, 14.5, 11]
