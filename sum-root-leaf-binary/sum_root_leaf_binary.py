2d88890ec3ba57b49fff2ed7eccf7aa5169251ae
Michelle Chen <mich.chen.94@gmail.com>
2020-10-23 16:40:12 -0700

    DFS recursive solution

--- sum-root-leaf-binary/sum_root_leaf_binary.py
+++ sum-root-leaf-binary/sum_root_leaf_binary.py
@@ -7,3 +7,23 @@ from typing import TreeNode
 #         self.left = left
 #         self.right = right
 
+def sumRootToLeaf(root: TreeNode) -> int:
+        
+        if not root.left and not root.right:
+            return int(str(root.val), 2)
+        
+        binarys = []
+        
+        def dfs(node, string):
+            if node:
+                current = string + str(node.val)
+                # if node is a leaf
+                if not node.left and not node.right:
+                    binarys.append(current)
+                # if not a leaf, look at children
+                else:
+                    dfs(node.left, current)
+                    dfs(node.right, current)
+                    
+        dfs(root, '')
+        return sum(int(string,2) for string in binarys)
