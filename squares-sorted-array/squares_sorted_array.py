747f77d38d55e82e85c9860ee06ae8e9439ca1ee
Michelle Chen <mich.chen.94@gmail.com>
2020-10-24 01:00:43 -0700

    O(n) solution

--- squares-sorted-array/squares_sorted_array.py
+++ squares-sorted-array/squares_sorted_array.py
@@ -2,12 +2,55 @@ from typing import List
 
 def sortedSquares(A: List[int]) -> List[int]:
 
-    # O(n log n) solution
-    output = []
-    for num in A:
-        output.append(num*num)
+    # # O(n log n) solution
+    # output = []
+    # for num in A:
+    #     output.append(num*num)
 
-    return sorted(output)
+    # return sorted(output)
+
+    # O(n) solution
+
+    output = []
+    
+    #if all neg or positive, just square everything and return
+    if A[0] >= 0:
+        for num in A:
+            output.append(num**2)
+        return output
+    
+    if A[-1] < 0:
+        for num in A[::-1]:
+            output.append(num**2)
+        return output
+
+    # 1st pass to set pointers to first positive num and largest neg num
+    left = right = 0
+    for i in range(len(A)):
+        if A[i] >= 0:
+            right = i
+            left = i - 1
+            break
+            
+    while left >= 0 and right < len(A):
+        # append the square of whichever abs value of num is smaller
+        if abs(A[left]) < abs(A[right]):
+            output.append(A[left] ** 2)
+            left -= 1
+        else: # A[right] <= A[left]
+            output.append(A[right] ** 2)
+            right += 1
+    # append remaining of beginning
+    while left >= 0:
+            output.append(A[left] ** 2)
+            left -= 1
+    # append remaining of end
+    while right < len(A):
+            output.append(A[right] ** 2)
+            right += 1
+        
+    return output
+
 
 
 if __name__ == '__main__':
