cde8b015e9467ad33fc73297f83d49b1ba6885c1
Michelle Chen <mich.chen.94@gmail.com>
2020-10-25 15:41:48 -0700

    alternative helper DFS function solutions

--- max-depth-n-ary-tree/max_depth_n-ary_tree.py
+++ max-depth-n-ary-tree/max_depth_n-ary_tree.py
@@ -8,11 +8,36 @@ class Node:
 
 def maxDepth(root: 'Node') -> int:
         
-        if not root:
-            return 0
-        
-        # if only one node in tree
-        if not root.children:
-            return 1
+    if not root:
+        return 0
     
-        return max([1 + maxDepth(child) for child in root.children])
+    # if only one node in tree
+    if not root.children:
+        return 1
+
+    return max([1 + maxDepth(child) for child in root.children])
+
+# ***** add helper DFS function *****
+
+    # def dfs(node):
+        
+        # for child in node.children:
+        #     return max(1 + dfs(child))
+        # # return max(1 + dfs(child) for child in node.children)
+    
+    # return dfs(root)
+
+# **** add helper DFS function passing depth *****
+
+    # def dfs(node, depth):
+#       # if leaf
+    #     if not node.children:
+    #         return depth
+    #     maximum = 1
+    #     for child in node.children: 
+    #         maximum = max(maximum, dfs(child, depth + 1))
+    #     return maximum
+
+# adding list comprehesion 
+    # return max([dfs(child, depth + 1) for child in node.children])
+
