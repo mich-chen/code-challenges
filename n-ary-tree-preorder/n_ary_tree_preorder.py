789a68c420b66740504c38c329885db690d1126b
Michelle Chen <mich.chen.94@gmail.com>
2020-10-25 14:55:41 -0700

    preorder recursively traversal

---
+++ n-ary-tree-preorder/n_ary_tree_preorder.py
@@ -0,0 +1,24 @@
+from typing import List
+
+"""
+# Definition for a Node.
+class Node:
+    def __init__(self, val=None, children=None):
+        self.val = val
+        self.children = children
+"""
+
+def preorder(root: 'Node') -> List[int]:
+    """preorder is self, left, right"""
+
+    output = []
+
+    def recurse(node):
+        if node:
+            output.append(node.val)
+            # children are list of nodes
+            for child in node.children:
+                recurse(child)
+
+    recurse(root)
+    return output
