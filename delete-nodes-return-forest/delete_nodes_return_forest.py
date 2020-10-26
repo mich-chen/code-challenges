8e9a44b8f63cb1bbdb0b18c738f2bc5b7a15af40
Michelle Chen <mich.chen.94@gmail.com>
2020-10-25 17:05:50 -0700

    cleaner DFS

--- delete-nodes-return-forest/delete_nodes_return_forest.py
+++ delete-nodes-return-forest/delete_nodes_return_forest.py
@@ -26,24 +26,41 @@ def delNodes(root: TreeNode, to_delete: List[int]) -> List[TreeNode]:
     output = []
 
     def dfs(node, parent, to_delete):
-        if node:
-        # delete node that is in to_delete
-        # edge case: if root is in to_delete, then parent is None
-            if node.val in to_delete and parent != None:
-                if parent.left == node:
-                    parent.left = None
-                if parent.right == node:
-                    parent.right = None
-        # to know if new subtree:
-        # current node is not in to_delete
-        # and parent node is in to_delete
-        # edge case: if root is in to_delete, then parent is None
-        if (parent == None or parent.val in to_delete) and node not in to_delete:
+        # if new subtree: parent is in to_delete, self is not in to_delete
+        # edge case: if orig root in to_delete, then parent == None
+        if not node:
+            return node
+        if (parent == None or parent.val in to_delete) and node.val not in to_delete:
             output.append(node)
-        # continue to traverse
-        dfs(node, parent, to_delete)
-        dfs(node, parent, to_delete)
-
+        node.left = dfs(node.left, node, to_delete)
+        node.right = dfs(node.right, node, to_delete)
+        return None if node.val in to_delete else node
+        
     dfs(root, None, to_delete)
     return output
+
+
+# **** dfs less clean *******
+
+    # def dfs(node, parent, to_delete):
+    #     if node:
+    #     # delete node that is in to_delete
+    #     # edge case: if root is in to_delete, then parent is None
+    #         if node.val in to_delete and parent != None:
+    #             if parent.left == node:
+    #                 parent.left = None
+    #             if parent.right == node:
+    #                 parent.right = None
+    #     # to know if new subtree:
+    #     # current node is not in to_delete
+    #     # and parent node is in to_delete
+    #     # edge case: if root is in to_delete, then parent is None
+    #     if (parent == None or parent.val in to_delete) and node not in to_delete:
+    #         output.append(node)
+    #     # continue to traverse
+    #     dfs(node, parent, to_delete)
+    #     dfs(node, parent, to_delete)
+
+    # dfs(root, None, to_delete)
+    # return output
 
