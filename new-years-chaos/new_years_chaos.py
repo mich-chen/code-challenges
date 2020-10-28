8c58cf9a1d9d0792d73d3c4ccd9219cf6376ab9e
Michelle Chen <mich.chen.94@gmail.com>
2020-10-28 00:16:39 -0700

    initial commit

--- new-years-chaos/new_years_chaos.py
+++ new-years-chaos/new_years_chaos.py
@@ -12,3 +12,31 @@ def minimumBribes(q):
         >>> q = 2, 5, 1, 3, 4
         'Too Chaotic' --> 5 cannot be in current position because swapped > 2 times
     """
+
+    bribes = 0
+    # track the sorted index of each element
+    qsorted = {x: i for i,x in enumerate(sorted(q))}
+    print(qsorted)
+
+    # iterate through queue, if any number has swapped > 2 times then return too chaotic
+    for i, element in enumerate(q):
+
+        if qsorted[element] - i > 2:
+            return 'Too chaotic'
+
+        elif i != 0 and q[i-1] < element:
+            # bribes += (qsorted[q[i-1]] - (i-1))
+            # continue
+        else:
+            bribes += (qsorted[element] - i)
+
+    return bribes
+
+
+if __name__ == '__main__':
+    
+
+    print(minimumBribes([2, 1, 5, 3, 4]))  # 3
+    print(minimumBribes([2, 5, 1, 3, 4])) # Too chaotic
+    print(minimumBribes([5, 1, 2, 3, 7, 8, 6, 4]))  # Too chaotic
+    print(minimumBribes([1, 2, 5, 3, 7, 8, 6, 4])) # 7
