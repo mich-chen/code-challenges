87e325c6b939922d441beb3b7d2d9aac93e251e4
Michelle Chen <mich.chen.94@gmail.com>
2020-10-31 01:09:37 -0700

    fixed solution to slightly changed bubble sort, by checking range from where num's orig position to where it is now

--- new-years-chaos/new_years_chaos.py
+++ new-years-chaos/new_years_chaos.py
@@ -1,42 +1,27 @@
 def minimumBribes(q):
-    """
-    queue for roller coaster, starting from 1 --> len(q)
-    each element can only max swap 2 times. 
-    one element can bribe element in front of it to swap.
-    find the min num of bribes that took place for current q
-    Return min bribes, or 'Too Chaotic' if line config is not possible (based on these conditions)
-    test cases:
-        >>> q = 2, 1, 5, 3, 4
-        min bribes = 3
-
-        >>> q = 2, 5, 1, 3, 4
-        'Too Chaotic' --> 5 cannot be in current position because swapped > 2 times
-    """
-
+    
     bribes = 0
-    # track the sorted index of each element
-    qsorted = {x: i for i,x in enumerate(sorted(q))}
-    print(qsorted)
 
-    # iterate through queue, if any number has swapped > 2 times then return too chaotic
-    for i, element in enumerate(q):
-
-        if qsorted[element] - i > 2:
-            return 'Too chaotic'
-
-        elif i != 0 and q[i-1] < element:
-            # bribes += (qsorted[q[i-1]] - (i-1))
-            # continue
+    for i, num in enumerate(q):
+        # if num minus its position is greater than 2, then bribed too much
+        if num - (i + 1) > 2:
+            print('Too chaotic')
+            return
+        # check how many nums are greater in range from one position in front of current num's supposed position to one in front of where it is
+        # so look at how many bribes happened for current number to be where it is
+        # use max() in case any negative indexing
         else:
-            bribes += (qsorted[element] - i)
+            for j in range(max(num - 2, 0), i):
+                if q[j] > num:
+                    bribes += 1
 
-    return bribes
+    print(bribes)
 
 
 if __name__ == '__main__':
     
 
-    print(minimumBribes([2, 1, 5, 3, 4]))  # 3
-    print(minimumBribes([2, 5, 1, 3, 4])) # Too chaotic
-    print(minimumBribes([5, 1, 2, 3, 7, 8, 6, 4]))  # Too chaotic
-    print(minimumBribes([1, 2, 5, 3, 7, 8, 6, 4])) # 7
+    (minimumBribes([2, 1, 5, 3, 4]))  # 3
+    (minimumBribes([2, 5, 1, 3, 4])) # Too chaotic
+    (minimumBribes([5, 1, 2, 3, 7, 8, 6, 4]))  # Too chaotic
+    (minimumBribes([1, 2, 5, 3, 7, 8, 6, 4])) # 7
