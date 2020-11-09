af1628a22140a444a4d7b1c788ac12253cfe79fb
Michelle Chen <mich.chen.94@gmail.com>
2020-11-08 20:05:00 -0800

    recursion to build full binary trees by splitting all possible num of nodes between left and right

---
+++ all-possible-full-bt/all_possible_full_bt.py
@@ -0,0 +1,35 @@
+from typing import TreeNode
+
+# Definition for a binary tree node.
+# class TreeNode:
+#     def __init__(self, val=0, left=None, right=None):
+#         self.val = val
+#         self.left = left
+#         self.right = right
+
+def allPossibleFBT(N: int) -> List[TreeNode]:
+
+    # cannot make valid full binary tree with even N
+    if N % 2 == 0:
+        return []
+
+    def makeTree(n):
+        current = []
+        # base case if n is 1, return list of one tree node
+        if n <= 1:
+            return [TreeNode(0)]
+        # iterate through odd numbers from 1 to n
+        for i in range(1, n, 2):
+            # split all possibilities of nodes between left and right
+            left = makeTree(i)
+            right = makeTree(n - i - 1)
+            for k in range(len(left)):
+                for j in range(len(right)):
+                    # build full binary tree, add to current list
+                    new = Treenode(0)
+                    new.left = left[k]
+                    new.right = right[j]
+                    current.append(new)
+        return current
+
+    return makeTree(N)
