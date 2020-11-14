0d19661ec506c704afcdf57a9c5a8fc1aace6b6c
Michelle Chen <mich.chen.94@gmail.com>
2020-11-13 23:24:58 -0800

    O(1) space solution by reversing second half and then compare

--- palindrome-ll/palindrome_ll.py
+++ palindrome-ll/palindrome_ll.py
@@ -7,7 +7,7 @@ from typing import ListNode
 #         self.next = next
 
 def isPalindrome(head: ListNode) -> bool:
-
+    # base case
     if not head:
         return True
 
@@ -31,3 +31,22 @@ def isPalindrome(head: ListNode) -> bool:
 
     return True
     
+    # ***** O(1) Space complexity *****
+    # same code for base case and finding middle
+
+    # reverse second half in place
+    half = None
+    while slow:
+        temp = slow.next
+        slow.next = half
+        half = slow
+        slow = temp
+
+    # compare halves
+    while half:
+        if half.val != head.val:
+            return False
+        half = half.next
+        head = head.next
+
+    return True
