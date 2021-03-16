ec24549d00c11e7d7b99f8d2f4dd65ec3b629527
Michelle Chen <mich.chen.94@gmail.com>
2021-03-15 17:16:37 -0700

    recursive solution with dp and classes

---
+++ fibonacci_number/fib_number.py
@@ -0,0 +1,31 @@
+class Solution:
+    def fib(self, N: int) -> int:
+        
+        # cache already calculated f(N) values
+        # index in self.dp list represents N
+        # dp[N] represents calculated fibonacci value
+        self.dp = [None] * (N + 1)
+
+        return self.recurse(N, self.dp)
+    
+    def recurse(self, N, dp):
+        """Calculate fibonacci number recursively"""
+
+        if N <= 1:
+            return N
+        
+        # if dp[N] not cached yet, cache the fib value by recursing
+        if not self.dp[N]:
+            self.dp[N] = self.recurse(N-1, self.dp) + self.recurse(N-2, self.dp)
+        
+        # if cached fib number exists, return
+        return dp[N]
+
+if __name__ == '__main__':
+
+    example1 = Solution()
+    print(example1.fib(4)) # 3
+    example2 = Solution()
+    print(example2.fib(30)) # 832040
+
+
