dbd7c9dc7f24784c35730cf0864d300f277df1c1
Michelle Chen <mich.chen.94@gmail.com>
2021-03-22 12:20:44 -0700

    permutations1 with tracker and recursion solution

--- arrays/permutations-series/permutations.py
+++ arrays/permutations-series/permutations.py
@@ -23,9 +23,34 @@ class Solution:
         self.swap_recurse(nums, 0, len(nums))
         return self.collect
 
+# -------------- array tracker solution ----------------------------------------
+    def tracker_recurse(self, nums, current, sofar):
+        # similar to recursing in a tree, when we reach a leaf/completed current
+        if len(current) == len(nums):
+            self.collect.append(current.copy())
+
+        for n in nums:
+            # check if num is already in current permutation
+            if n in sofar:
+                continue
+            # add to sofar set as tracker, add to current permutation
+            sofar.add(n)
+            current.append(n)
+            self.tracker_recurse(nums, current, sofar)
+            # backtrack
+            sofar.remove(n)
+            current.pop()
+
+    def tracker_permutate(self, nums: List[int]) -> List[List[int]]:
+        self.collect = []
+        # sofar as a set because nums contains unique elements
+        self.tracker_recurse(nums, [], set())
+        return self.collect
+
 
 if __name__ == '__main__':
 
     ex1 = Solution().swap_permutate([1,2,3]) 
     print(ex1) # [[1, 2, 3], [1, 3, 2], [2, 1, 3], [2, 3, 1], [3, 2, 1], [3, 1, 2]]
-    
+    ex2 = Solution().tracker_permutate([1,2,3])
+    print(ex2) # [[1, 2, 3], [1, 3, 2], [2, 1, 3], [2, 3, 1], [3, 1, 2], [3, 2, 1]]
