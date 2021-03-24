70963af13dc65cb19fcf43f275c9b884eb6db66d
Michelle Chen <mich.chen.94@gmail.com>
2021-03-24 16:47:06 -0700

    iterative solution again, more condensed, use list slicing for reversing and additional loop through length of queue

--- trees/bt-zig-zag-lvl-traversal/bt_zig_zag_lvl_traversal.py
+++ trees/bt-zig-zag-lvl-traversal/bt_zig_zag_lvl_traversal.py
@@ -9,6 +9,40 @@ from collections import deque
 
 class Solution:
     def zigzagLevelOrder(self, root: TreeNode) -> List[List[int]]:
+
+
+# ------------------------------------------------------------------------------
+
+
+        # iterative - condensed, loops through level list
+        #           - use -1 as direction for list slicing for reversing
+        if not root:
+            return []
+
+        q = deque([root])
+        result = []
+        direction = 1
+
+        while q:
+            lvl = []
+            # iterates through current queue (current level)
+            for i in range(len(q)):
+                node = q.popleft()
+                # add node value to levels list
+                lvl.append(node.val)
+                # traverse, new level in queue will evaluate length at next while-loop iteration
+                if node.left:
+                    q.append(node.left)
+                if node.right:
+                    q.append(node.right)
+            # add lvl to result in forward or reverse using direction and slicing
+            result.append(lvl[::direction])
+            direction *= (-1)
+        
+        return result
+
+# ------------------------------------------------------------------------------
+
         # iterateively - initial attempt, no nested loops
         if not root:
             return []
