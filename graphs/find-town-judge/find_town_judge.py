0b6a2705507e912f80d5864f3b7ba7501387aa44
Michelle Chen <mich.chen.94@gmail.com>
2021-03-27 02:03:13 -0700

    add alternative solution using dictionary and set intersection to find town judge

--- graphs/find-town-judge/find_town_judge.py
+++ graphs/find-town-judge/find_town_judge.py
@@ -1,7 +1,7 @@
 from typing import List
 
 def findJudge(N: int, trust: List[List[int]]) -> int:
-
+    """Indegree - outdegree connected graph solution."""
     # initialize an array to keep track of trust count for each person
     # N + 1 because indexing, and each person starts at 1
     trusts = [0] * (N + 1)
@@ -22,8 +22,36 @@ def findJudge(N: int, trust: List[List[int]]) -> int:
 
     return -1
 
+def findJudge_sets(N: int, trust: List[List[int]]) -> int:
+    """Use dictionary and set intersection solution."""
+    # base case: if only townjudge and no trusts
+    if not trust and N == 1:
+        return N
+
+    # keys = person, value = set of people they trust
+    d = {i: {i} for i in range(1, N + 1)}
+
+    # if townjudge exists, will not be part of iteration
+    for person, trusts in trust:
+        d[person].add(trusts)
+        # remove self from trusted ppl
+        # townjudge trusts "themselves" & will exist in set intersection check later
+        if person in d[person]:
+            d[person].remove(person)
+    
+    # initialize town judge with first person's trusted ppl
+    town_judge = d[1]
+    # intersect sets, only return the common person across all sets
+    for trusted in d.values():
+        town_judge = town_judge&trusted
+
+    return town_judge.pop() if len(list(town_judge)) == 1 else -1
+
 
 if __name__ == '__main__':
     
     print(findJudge(3, [[1,3],[2,3],[3,1]]))  # -1
     print(findJudge(4, [[1,3],[1,4],[2,3],[2,4],[4,3]])) # 3
+    print(findJudge_sets(3, [[1,3],[2,3],[3,1]])) # -1
+    print(findJudge_sets(4, [[1,3],[1,4],[2,3],[2,4],[4,3]])) # 3
+    
