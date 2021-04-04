43df0aca181726aed34d208d5d4c802b9286091a
Michelle Chen <mich.chen.94@gmail.com>
2021-04-03 21:35:20 -0700

    recursive solution using inorder traversal

--- trees/min-absolute-diff-bst/min_absolute_diff_bst.py
+++ trees/min-absolute-diff-bst/min_absolute_diff_bst.py
@@ -16,4 +16,28 @@ class Solution:
             return min(left, right)
         return fn(root, float('-inf'), float('inf'))
 
-    
+    def getMinDiff_inorder(self, root: TreeNode) -> int:
+        """Recursively - inorder traversal."""
+
+        def traverse(node, dp):
+            if not node:
+                return dp
+            # inorder traversal --> left, self, right
+            left = traverse(node.left, dp)
+            min_diff = dp[1]
+            # calculate min diff for children, if not at root
+            #   self - left child (stored as dp[0])
+            #   or self - right child
+            if dp[0] != -1:
+                min_diff = min(min_diff, node.val - dp[0])
+            # update dp[0] as self for calculating self - right child
+            dp[0], dp[1] = node.val, min_diff
+            return traverse(node.right, dp)
+        
+        # store data as list where:
+        #   list[0] = node to be subtracted
+        #   list[1] = min diff
+        dp = [-1, float('inf')]
+        output = traverse(root, dp)
+        return output[1]
+
