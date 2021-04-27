51ee730def37b337d8f1ac9c429ef76ecd4ea24d
Michelle Chen <mich.chen.94@gmail.com>
2021-04-27 12:50:31 -0700

    recursive solution

--- arrays/pascals-triangle/pascals_triangle.py
+++ arrays/pascals-triangle/pascals_triangle.py
@@ -2,7 +2,22 @@ from typing import List
 
 class Solution:
     def generate(self, numRows: int) -> List[List[int]]:
-        
+        """Recursive solution"""
+        # base edge case
+        if numRows == 0:
+            return []
+
+        def recurse(rows, triangle):
+            # base case
+            if rows == 0:
+                return triangle
+            triangle.append(self.calculate(triangle[-1]))
+            return recurse(rows - 1, triangle)
+
+        return recurse(numRows - 1, [[1]])
+
+    def generate_iterative(self, numRows: int) -> List[List[int]]:
+        """Iterative for-loop solution"""
         if numRows == 0:
             return []
         
@@ -22,6 +37,10 @@ class Solution:
 
 if __name__ == '__main__':
     example = Solution()
+    print(example.generate_iterative(5)) # [[1],[1,1],[1,2,1],[1,3,3,1],[1,4,6,4,1]]
+    print(example.generate_iterative(1)) # [[1]]
+    print(example.generate_iterative(0)) # [[]]
+
     print(example.generate(5)) # [[1],[1,1],[1,2,1],[1,3,3,1],[1,4,6,4,1]]
     print(example.generate(1)) # [[1]]
     print(example.generate(0)) # [[]]
