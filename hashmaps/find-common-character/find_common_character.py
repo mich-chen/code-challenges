2db242d319b9f42564fa1d0e5fc4d656633e99cd
Michelle Chen <mich.chen.94@gmail.com>
2021-05-02 18:54:42 -0700

    add non-hashmap solution by using alphabet bucket to keep track of min occurences

--- hashmaps/find-common-character/find_common_character.py
+++ hashmaps/find-common-character/find_common_character.py
@@ -23,7 +23,34 @@ class Solution:
 
         return result
 
+    def commonChars_bucket(self, A: List[str]) -> List[str]:
+
+        # alphabet bucket
+        bucket = [float('inf')] * 26
+
+        # iterate through each word
+        for word in A:
+            # create word count using bucket
+            word_ct = [0] * 26
+            for ch in word:
+                word_ct[ord(ch) - ord('a')] += 1
+
+            # iterate through alphabet and update min occurence of each letter
+            # if letter is not present in other words, then it will be 0 in bucket
+                # indicating it is not a common character
+            for i in range(26):
+                bucket[i] = min(bucket[i], word_ct[i])
+
+        result = []
+        # iterate through bucket, if count is 1 or greater, then add to result list with number of occurrences
+        for i,count in enumerate(bucket):
+            if count >= 1:
+                result.extend([chr(i + 97)] * count)
+        return result
+            
+
 if __name__ == '__main__':
 
     example = Solution()
     print(example.commonChars(["bella","label","roller"])) # ["e","l","l"]
+    print(example.commonChars_bucket(["bella","label","roller"])) # ["e","l","l"]
