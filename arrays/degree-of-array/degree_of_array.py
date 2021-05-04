c6f56b57e1a2ef606c3a45119b669ead37099d92
Michelle Chen <mich.chen.94@gmail.com>
2021-05-03 22:53:53 -0700

    add single pass solution

--- arrays/degree-of-array/degree_of_array.py
+++ arrays/degree-of-array/degree_of_array.py
@@ -22,8 +22,25 @@ class Solution:
 
         return smallest_length
 
+    def findShortestSubArray_single_pass(self, nums: List[int]) -> int:
+        
+        indexes = defaultdict(list)
+        degree = 0
+        smallest_length = len(nums)
+        for i, n in enumerate(nums):
+            indexes[n].append(i)
+            if len(indexes[n]) == degree:
+                smallest_length = min(smallest_length, indexes[n][-1] - indexes[n][0] + 1)
+            elif len(indexes[n]) > degree:
+                degree = len(indexes[n])
+                smallest_length = indexes[n][-1] - indexes[n][0] + 1
+
+        return smallest_length
+
+
 if __name__ == '__main__':
 
     example = Solution()
     print(example.findShortestSubArray([1,2,2,3,1])) # 2
     print(example.findShortestSubArray([1,2,2,3,1,4,2])) # 6
+    print(example.findShortestSubArray_single_pass([1,2,2,3,1,4,2]))
