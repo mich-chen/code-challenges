348359eca1cdb7e24f8003dfb1cb37cbaa507bbf
Michelle Chen <mich.chen.94@gmail.com>
2021-05-03 20:17:05 -0700

    solution using constant space, use variables as way to track previously calculated ways per step

--- dynamic-programming/climbing-stairs/climbing_stairs.py
+++ dynamic-programming/climbing-stairs/climbing_stairs.py
@@ -20,8 +20,29 @@ class Solution:
 
         return dp[-1]
 
+    def climbStairs_constant_space(self, n: int) -> int:
+
+        # base case 
+        if n <= 2:
+            return n
+
+        one_step_previous = 1
+        two_step_previous = 2
+        ways = 2 # initialize ways as 2 since n is > 2
+
+        # use pointers to indicate num of ways one step previous and two step previous
+        for i in range(2, n):
+            ways = one_step_previous + two_step_previous
+            # move pointers accordingly
+            one_step_previous = two_step_previous
+            two_step_previous = ways # ways now represents i - 1 for next interation
+
+        return ways
+
 if __name__ == '__main__':
 
     example = Solution()
     print(example.climbStairs(5)) # 8
     print(example.climbStairs(38)) # 63245986
+    print(example.climbStairs_constant_space(5))
+    print(example.climbStairs_constant_space(38))
